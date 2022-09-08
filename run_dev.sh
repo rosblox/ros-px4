df6a6f6fbb7b4f241ab92c7c818ae194df5721f5
Max <max@seervision.com>
2022-09-08 09:27:25 +0200

    Update submodule.

--- run_dev.sh
+++ run_dev.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
 docker run -it --privileged  -v /dev/shm:/dev/shm --shm-size=256m --net=host -v $(pwd)/px4_msgs:/colcon_ws/src/px4_msgs -v $(pwd)/px4_ros_com:/colcon_ws/src/px4_ros_com ghcr.io/rosblox/ros-px4:humble
-
-# colcon build --cmake-args --symlink-install --packages-skip px4_msgs --event-handlers console_direct+
-# ros2 run px4_ros_com offboard_control
