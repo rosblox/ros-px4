076980a0a44837001fce4b1a46dfb888a31952e0
Max <max@seervision.com>
2022-11-09 14:56:41 +0100

    Remove unused variables

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-docker run -it --privileged  --workdir /colcon_ws -v /dev/shm:/dev/shm --shm-size=256m --net=host -v $(pwd)/px4_msgs:/colcon_ws/src/px4_msgs -v $(pwd)/px4_ros_com:/colcon_ws/src/px4_ros_com ghcr.io/rosblox/ros-px4:humble
-
+docker run -it --workdir /colcon_ws -v $(pwd)/px4_msgs:/colcon_ws/src/px4_msgs -v $(pwd)/px4_ros_com:/colcon_ws/src/px4_ros_com ghcr.io/rosblox/ros-px4:humble
