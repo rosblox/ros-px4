c6a57d3e9173d2921f2013266421a5d3048bd8b4
Max Polzin <polzin.max@gmail.com>
2022-11-09 16:23:27 +0100

    Update with warning.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --net=vortex_default --workdir /colcon_ws -v $(pwd)/px4_msgs:/colcon_ws/src/px4_msgs -v $(pwd)/px4_ros_com:/colcon_ws/src/px4_ros_com ghcr.io/rosblox/ros-px4:humble
+docker run -it --net=host --workdir /colcon_ws -v $(pwd)/px4_msgs:/colcon_ws/src/px4_msgs -v $(pwd)/px4_ros_com:/colcon_ws/src/px4_ros_com ghcr.io/rosblox/ros-px4:humble
